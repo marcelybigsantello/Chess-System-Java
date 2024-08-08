6ec894a9999b488f50dd3782624819e60f1856c9
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2024-08-07 22:37:12 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,22 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int column;
+	
+	public Position() {
+		
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return  row + ", " + column;
+	}
+	
+}
