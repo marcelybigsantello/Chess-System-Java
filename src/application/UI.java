0f4b1cdd8681754733b76e83593d45bdd6b7d984
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2024-08-19 22:46:17 -0300

    Clearing screen and handling exceptions without interrupting the execution

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,6 +8,11 @@ import chess.ChessPosition;
 
 public class UI {
 	
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
